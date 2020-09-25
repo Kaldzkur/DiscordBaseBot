1f272a79a59d40d6242819529f49572341d81b63
alexP55 <AlxPn9696@gmail.com>
2020-09-25 15:27:11 -0700

    signal to force kill

--- shutdown.sh
+++ shutdown.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 if [ -z "$1" ] ; then
   #No input argument passed -> kill all base_bot instances
-  sudo pkill -f base_bot.py
+  sudo pkill -9 -f base_bot.py
   #wait for all processes to end
   PID=$(/usr/bin/pgrep -f base_bot.py)
   while [ "$PID" != "" ];do
     sleep 1
-    sudo pkill -f base_bot.py
+    sudo pkill -9 -f base_bot.py
     PID=$(/usr/bin/pgrep -f base_bot.py)
   done
 else
   #kill only the process with the given pid (arg 1 passed to script)
-  sudo kill $1
-  #wait for all processes to end
-  PID=$(/usr/bin/pgrep -f base_bot.py)
-  while [ "$PID" != "" ];do
-    sleep 1
-    sudo kill $1
-    PID=$(/usr/bin/pgrep -f base_bot.py)
-  done
+  sudo kill -9 $1
 fi
