96360b511ac29c419862942dc37ab0a22614463b
Kaldzkur <68599904+Kaldzkur@users.noreply.github.com>
2020-09-25 22:23:20 +0200

    Only shutdown the current bot instance

--- shutdown.sh
+++ shutdown.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
-#kill all python3 processes
-sudo pkill -9 python3
-#wait for all processes to end
-PID=$(/usr/bin/pgrep python3)
-while [ "$PID" != "" ];do
-  sleep 1
-  sudo pkill -9 python3
-  PID=$(/usr/bin/pgrep python3)
-done
+if [ -z "$1" ] ; then
+  #No input argument passed -> kill all base_bot instances
+  sudo pkill -f base_bot.py
+  #wait for all processes to end
+  PID=$(/usr/bin/pgrep -f base_bot.py)
+  while [ "$PID" != "" ];do
+    sleep 1
+    sudo pkill -f base_bot.py
+    PID=$(/usr/bin/pgrep -f base_bot.py)
+  done
+else
+  #kill only the process with the given pid (arg 1 passed to script)
+  sudo kill $1
+  #wait for all processes to end
+  PID=$(/usr/bin/pgrep -f base_bot.py)
+  while [ "$PID" != "" ];do
+    sleep 1
+    sudo kill $1
+    PID=$(/usr/bin/pgrep -f base_bot.py)
+  done
+fi
