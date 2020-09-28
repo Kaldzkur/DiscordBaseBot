bf941a10b738463da691db10ec429f7142652d6f
alexP55 <AlxPn9696@gmail.com>
2020-09-28 14:18:45 -0700

    allow prefix to be @bot or @bot-role

--- base_bot.py
+++ base_bot.py
@@ -28,14 +28,14 @@ class BaseBot(commands.Bot):
     @self.check # add a global check to the bot
     def check_initialized(context):
       if context.guild.id not in context.bot.intialized or not context.bot.intialized[context.guild.id]:
-        raise commands.CheckFailure("Guild {context.guild.name} is not initialized")
+        raise commands.CheckFailure(f"Guild {context.guild.name} is not initialized")
       return True
 
   async def find_prefix(self, message):
     prefix = await self.get_prefix(message)
-    if type(prefix) == list:
+    if isinstance(prefix, (list, tuple)):
       for pre in prefix:
-        if message.startswith(pre):
+        if message.content.startswith(pre):
           return pre
       else:
         return None
@@ -800,12 +800,18 @@ class BaseBot(commands.Bot):
     return self.get_guild(self.main_server_id)
     
 def dynamic_prefix(bot, message):
-  if message.type != discord.MessageType.default:
-    return
-  if hasattr(message, "guild") and message.guild:
-    return bot.get_guild_prefix(message.guild)
+  if hasattr(message, "guild"):
+    prefix_list = []
+    if message.guild.id in bot.intialized and bot.intialized[message.guild.id]:
+      prefix_list.append(bot.get_guild_prefix(message.guild))
+      role = bot.get_bot_role(message.guild)
+      if role:
+        prefix_list.append(f"{role.mention} ")
+    prefix_list += [f"{bot.user.mention} ", f"<@!{bot.user.id}> "]
   else: #in DMs the bot can respond to prefix-less messages
-    return "?", ""
+    prefix_list = ["?", ""]
+  return prefix_list
+  
 
 if __name__ == "__main__":
   import os
