1222d84d5113f5e4d1325370f2df86780e8f2627
alexP55 <AlxPn9696@gmail.com>
2021-10-20 15:20:02 -0700

    interactive selection class

--- base/modules/interactive_message.py
+++ base/modules/interactive_message.py
@@ -1,7 +1,8 @@
 import discord
+from datetime import datetime
 from abc import ABC, abstractmethod
 import asyncio
-from base.modules.constants import arrow_emojis
+from base.modules.constants import arrow_emojis, num_emojis
 
 class InteractiveMessage(ABC):
   
@@ -151,6 +152,73 @@ class DetermInteractiveMessage(InteractiveMessage, ABC):
     else:
       return None
     return new_msg
+            
+class InteractiveSelectionMessage(InteractiveMessage):
+  # Selections
+  def __init__(self, selections, transfer, parent=None, **attributes):
+    assert callable(transfer), "The input transfer must be a function"
+    assert selections and isinstance(selections, list), "selections must be a non-empty list"
+    super().__init__(parent, **attributes)
+    self.selections = selections
+    self.num = len(selections)
+    self.trans = transfer
+    self.page_length = attributes.pop("page_length", 10)
+    self.title = attributes.pop("title", "Please Make a Selection")
+    self.description = attributes.pop("description", "")
+    self.page = 1
+    self.total_page = self.num//self.page_length + 1
+    if self.total_page > 1:
+      self.child_emojis += [arrow_emojis["backward"], arrow_emojis["forward"]]
+    longest_page = min(self.page_length, self.num)
+    self.child_emojis += num_emojis[1:longest_page+1]
+    
+  async def transfer_to_child(self, emoji):
+    if emoji == arrow_emojis["backward"]:
+      self.page = max(self.page - 1, 1)
+      return self
+    elif emoji == arrow_emojis["forward"]:
+      self.page = min(self.page + 1, self.total_page)
+      return self
+    else:
+      ind = num_emojis.index(emoji) - 1
+      selection = ind + (self.page - 1) * self.page_length
+      if selection >= self.num:
+        return None
+      child = self.trans(selection)
+      child.set_parent(self)
+      return child
+      
+  async def get_embed(self):
+    page_selections = self.selections[(self.page-1)*10:self.page*10]
+    page_selections = [f"{num_emojis[ind+1]} {page_selections[ind]}" for ind in range(0,len(page_selections))]
+    selection_content = "\n".join(page_selections)
+    description = f"{self.description}\n{selection_content}"
+    embed = discord.Embed(title=self.title, timestamp=datetime.utcnow(), description=description)
+    embed.set_footer(text=f"Page {self.page}/{self.total_page}")
+    return embed
+      
+            
+class InteractiveEndMessage(InteractiveMessage):
+  # End message, can still be reversed to its parent
+  def __init__(self, parent=None, **attributes):
+    super().__init__(parent, **attributes)
+    self.content = attributes.pop("content", None)
+    self.embed = attributes.pop("embed", None)
+    self.file = attributes.pop("file", None)
+    
+  async def get_content(self): # return some content
+    return self.content
+  
+  async def get_embed(self): # return an embed
+    return self.embed
+  
+  async def get_file(self): # return a file
+    return self.file
+    
+  async def transfer_to_child(self, emoji):
+    pass
+  
+  
     
     
 async def update_reactions(message, old_emojis, new_emojis, reaction, user):
